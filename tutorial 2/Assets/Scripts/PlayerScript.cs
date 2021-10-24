f0c8828607185d449e7e85aaa24732831f26db54
capt-butter <captbutterspam@gmail.com>
2021-10-24 11:29:40 -0400

    finished challenge

--- tutorial 2/Assets/Scripts/PlayerScript.cs
+++ tutorial 2/Assets/Scripts/PlayerScript.cs
@@ -16,15 +16,33 @@ public class PlayerScript : MonoBehaviour
     public Text livestxt;
 
     public Text winmsg;
-
+    public Text losemsg;
+    //audio stuff
+    public AudioSource musicSource;
+    public AudioClip background;
+    public AudioClip win;
+    //animation stuff
+    Animator anim;
+    private bool isright = true;
+    private bool isOnGround;
+    public Transform groundcheck;
+    public float checkRadius;
+    public LayerMask allGround;
+    public bool won = false;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
-        livestxt.text = lives.ToString();
+        livestxt.text = "lives: " + lives.ToString();
         winmsg.text = "";
+        losemsg.text = "";
+        anim = GetComponent<Animator>();
+
+        //starts up audio
+        musicSource.clip = background;
+        musicSource.Play();
     }
 
     // Update is called once per frame
@@ -37,32 +55,85 @@ public class PlayerScript : MonoBehaviour
         {
             Application.Quit();
         }
-        if (scoreValue == 2)
+        
+        
+        else if (isright == true && hozMovement < 0)
         {
-            winmsg.text = "hi";
+            Flip();
         }
-    }
+        if (isright == false && hozMovement > 0)
+        {
+            Flip();
+        }
+        if (hozMovement != 0)
+        {
+            anim.SetInteger("state", 1);
+        }
+        else if (hozMovement == 0 && vertMovement == 0)
+        {
+            anim.SetInteger("state", 0);
+        }
+        isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);      
 
-    
+    }
+    //collision detection
     private void OnCollisionEnter2D(Collision2D collision)
     {
-       if (collision.collider.tag == "Coin")
+        //if player hits a coin this happens
+        if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
-        }
+            //checks if at score to move to 2nd lvl
+            if (scoreValue == 4)
+            {
+                transform.position = new Vector2(85.0f, 5.0f);
+                lives = 3;
+                livestxt.text = "lives: " + lives.ToString();
+            }
+            if (scoreValue == 8 && won == false)
+            {
+                won = true;
+                winmsg.text = "YOU WON, MADE BY ETHAN SYMONDS";
+                musicSource.Stop();
+                musicSource.PlayOneShot(win, 1.0f);
+                musicSource.clip = background;
+                musicSource.Play();
 
+            }
+        }
+        //if player hits baddie this happens
+        if (collision.collider.tag == "enemy")
+        {
+            lives = lives - 1;
+            livestxt.text = "lives: " + lives.ToString();
+            Destroy(collision.collider.gameObject);
+            if (lives == 0)
+            {
+                losemsg.text = "you lost, try again";
+                Destroy(gameObject);
+            }
+        }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Ground")
+        if (collision.collider.tag == "Ground" && isOnGround)
         {
             if (Input.GetKey(KeyCode.W))
             {
-                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+                anim.SetInteger("state", 2);
+                rd2d.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+                anim.SetInteger("state", 2);
             }
         }
+    }
+    void Flip()
+    {
+        isright = !isright;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
     }
 }
